73b1d01797a55f10c38dbcef9925f342eec232d9
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2023-09-17 08:21:32 +0200

    Update ext-scrollbar.js

--- src/ext-scrollbar.js
+++ src/ext-scrollbar.js
@@ -1,3 +1,24 @@
+/*
+original:
+https://github.com/ukandrewc/Ace-Scrollbars
+https://github.com/ukandrewc/Ace-Scrollbars/blob/master/src/ext-scrollbar.js
+
+changes made by luberth
+https://github.com/ldijkman/randomnerd_esp32_wifi_manager/blob/main/docs/mrs/Ace_color/ext-scrollbar.js
+https://plnkr.co/plunk/EYJWlWGjCdjNoui8
+https://plnkr.co/edit/Digj5Fw7D6hxaTU4
+
+my editor does not start at mousepos/windowpos y zero
+so i treid to make an offset for that
+
+problem with scrollbar viewport on fontsize
+only seems to work ok at fontsize 12
+treid to change from pixel positions to percent positions
+not perfect yet
+but works a bit better i think
+
+*/
+
 function AceScrollbars(e) {
 
 	// Add CSS scrollbars
@@ -34,6 +55,19 @@ function AceScrollbars(e) {
 		h = setTimeout(showMapItems, 250)
 	})
 
+///////////////////////////////////////////////////////////////////////////
+// arduino alike map function == Re-maps a number from one range to another. 
+// https://www.arduino.cc/reference/en/language/functions/math/map/ 
+// https://stackoverflow.com/questions/5649803/remap-or-map-function-in-javascript
+// scale range 1 into range2 bar range to 0 to 1440 minute range  
+// or 
+// scale a minutevalue in range 0 to 1440 minutes to 0% to 100%,  used for left and width of timeslot divs
+///////////////////////////////////////////////////////////////////////////////
+function map_range(value, low1, high1, low2, high2) {
+    return (low2 + (high2 - low2) * (value - low1) / (high1 - low1)).toFixed(3);   // to 3 decimals, Math.round did not scale right
+}
+
+
 	showMapItems = function () {
 		let ht = ""
 		let tx = e.getCopyText()
@@ -43,32 +77,43 @@ function AceScrollbars(e) {
 			e.$search.setOptions(op)
 			let lr = -1
 			for (r of e.$search.findAll(e.session)) {
-				if (r.start.row != lr) {
-					ht += "<div class='ace_map-item' style='top:" + (r.start.row * rScale) + "px'></div>"
+				if (r.start.row != lr) {                                                     // 40 lines for editor view?
+					ht += "<div class='ace_map-item' style='top:" +  (map_range(r.start.row ,0,editor.session.getLength()+50, 0,100)) + "%'></div>"
+					// luberth thinks, i do not know maybe better in % no px
+					console.log("rScale",rScale);
 					lr = r.start.row
 				}
 			}
 		}
 		mc.innerHTML = ht
-		mr.style.top = e.getCursorPosition().row * rScale + "px"
+		mr.style.top = map_range(e.getCursorPosition().row,0,editor.session.getLength()+50, 0,100)+"%"
+		console.log("rScale2",rScale);
 	}
 
 	// Vertical
 	let rScale = 1
+		
 	let vScale = 1
 	let vStart = -1
 
 	let vs = sc[0]
-	let tv = document.getElementById("ace_bar-v")
+	let tv = document.getElementById("ace_bar-v")    // tv is the gray view on scrollbar, should match the editor view position
 
-	vs.addEventListener("scroll", function () {
-		tv.style.top = vs.scrollTop * vScale + "px"
+	vs.addEventListener("scroll", function (e) {//think it needes an offset here if editor does not start at top 0 of document
+		tv.style.top = (vs.scrollTop-document.getElementById("editor").offsetTop) * vScale + "px"
+		tv.style.top = (map_range(vs.scrollTop-document.getElementById("editor").offsetTop,0,document.getElementById("editor").offsetBottom,0,100))+"%"
+		//tv.style.top = (vs.scrollTop-e.offsetTop) * vScale + "px"
+
 	})
 
 	// Drag events
 	tv.addEventListener("pointerdown", function (e) {
 		tv.setPointerCapture(e.pointerId)
 		vStart = e.clientY
+    		//console.log("e.clientY",e.clientY);
+    		//console.log("mc.offsetTop+'px'",mc.offsetTop);
+    		console.log("offsetTop",document.getElementById("editor").offsetTop);
+    
 	})
 
 	tv.addEventListener("pointermove", function (e) {
@@ -84,7 +129,10 @@ function AceScrollbars(e) {
 	})
 
 	tv.previousElementSibling.addEventListener("mousedown", function (e) {
-		vs.scrollTop = e.clientY / vScale
+		// my editor does not start at zero because of my non editor buttons menu so offset needed
+     		vs.scrollTop = (e.clientY-document.getElementById("editor").offsetTop) / vScale
+	   	//vs.scrollTop = (e.clientY-e.offsetTop) / vScale
+
 	})
 
 	// Horizontal
@@ -93,7 +141,7 @@ function AceScrollbars(e) {
 
 	let hs = sc[1]
 	let th = document.getElementById("ace_bar-h")
-
+/*
 	hs.addEventListener("scroll", function () {
 		th.style.left = (hs.scrollLeft * hScale) + gWidth + "px"
 	})
@@ -119,7 +167,7 @@ function AceScrollbars(e) {
 	th.previousElementSibling.addEventListener("mousedown", function (e) {
 		hs.scrollLeft = e.clientX / hScale
 	})
-
+*/
 	resizeScroll = function () {
 		vScale = vs.clientHeight / vs.scrollHeight
 		if (vs.clientHeight) {
@@ -129,7 +177,7 @@ function AceScrollbars(e) {
 		else {
 			showScroll(tv, false)
 		}
-
+/*
 		hScale = hs.clientWidth / hs.scrollWidth
 		if (hs.clientWidth) {
 			th.style.width = (hs.clientWidth * hScale) + "px"
@@ -139,8 +187,9 @@ function AceScrollbars(e) {
 		else {
 			showScroll(th, false)
 		}
-
+*/
 		rScale = e.renderer.lineHeight * vScale
+		console.log("e.renderer.lineHeight",e.renderer.lineHeight);
 		gWidth = e.renderer.gutterWidth
 		th.previousElementSibling.style.left = gWidth + "px"
 		showMapItems()
@@ -151,4 +200,8 @@ function AceScrollbars(e) {
 		e.style.display = d
 		e.previousElementSibling.style.display = d
 	}
+
+
+
+	resizeScroll() // luberth added this on startup scrollbar was not ok only after resize window it was ok
 }
