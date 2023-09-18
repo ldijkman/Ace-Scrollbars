bb8bf6fb179097ed0d8291226813f9aa6502f874
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2023-09-18 17:35:26 +0200

    Add files via upload

---
+++ docs/ext-scrollbar.js
@@ -0,0 +1,215 @@
+/*
+original:
+https://github.com/ukandrewc/Ace-Scrollbars
+https://github.com/ukandrewc/Ace-Scrollbars/blob/master/src/ext-scrollbar.js
+
+changes made by luberth
+https://github.com/ldijkman/randomnerd_esp32_wifi_manager/blob/main/docs/mrs/Ace_color/ext-scrollbar.js
+https://plnkr.co/plunk/EYJWlWGjCdjNoui8
+https://plnkr.co/edit/Digj5Fw7D6hxaTU4
+
+my editor does not start at mousepos/windowpos y zero
+so i treid to make an offset for that
+
+problem with scrollbar viewport on fontsize
+only seems to work ok at fontsize 12
+treid to change from pixel positions to percent positions
+not perfect yet
+but works a bit better i think
+
+https://m.facebook.com/luberth.dijkman
+
+*/
+
+function AceScrollbars(e) {
+
+	// Add CSS scrollbars
+	e.container.insertAdjacentHTML("beforeend", "<div id='ace_pre-v' class='ace_scroll-v'></div><div id='ace_bar-v' class='ace_scroll-v ace_thumb-v'></div>")
+	e.container.insertAdjacentHTML("beforeend", "<div id='ace_pre-h' class='ace_scroll-h'></div><div id='ace_bar-h' class='ace_scroll-h ace_thumb-h'></div>")
+	e.container.insertAdjacentHTML("beforeend", "<div id='ace_map-row' class='ace_map-row'></div>")
+	e.container.insertAdjacentHTML("beforeend", "<div id='ace_map'></div>")
+
+	// Ace scrollbars
+	let gWidth = 34
+	let sc = document.getElementsByClassName("ace_scrollbar")
+
+	let ro = new MutationObserver(function () {
+		resizeScroll()
+	})
+
+	// Resize
+	window.addEventListener("resize", function () {
+		resizeScroll()
+	})
+
+	ro.observe(e.container, { characterData: true })
+	for (s of sc) {
+		ro.observe(s.firstElementChild, { attributes: true })
+	}
+
+	// Map
+	let mr = document.getElementById("ace_map-row")
+	let mc = document.getElementById("ace_map")
+
+	let h = 0
+	e.on("changeSelection", function () {
+		clearTimeout(h)
+		h = setTimeout(showMapItems, 250)
+	})
+
+///////////////////////////////////////////////////////////////////////////
+// arduino alike map function == Re-maps a number from one range to another. 
+// https://www.arduino.cc/reference/en/language/functions/math/map/ 
+// https://stackoverflow.com/questions/5649803/remap-or-map-function-in-javascript
+// scale range 1 into range2 bar range to 0 to 1440 minute range  
+// or 
+// scale a minutevalue in range 0 to 1440 minutes to 0% to 100%,  used for left and width of timeslot divs
+///////////////////////////////////////////////////////////////////////////////
+function map_range(value, low1, high1, low2, high2) {
+    return (low2 + (high2 - low2) * (value - low1) / (high1 - low1)).toFixed(3);   // to 3 decimals, Math.round did not scale right
+}
+
+
+	showMapItems = function () {
+		let ht = ""
+		let tx = e.getCopyText()
+		if (tx != "") {
+			let op = e.getLastSearchOptions()
+			op.needle = e.getCopyText()
+			e.$search.setOptions(op)
+			let lr = -1	
+			let nr_lines_vissible = editor.renderer.getLastVisibleRow() - editor.renderer.getFirstVisibleRow(); 
+			for (r of e.$search.findAll(e.session)) {
+				if (r.start.row != lr) {       
+				                                           // nr_lines_vissible 40 lines for editor view?
+					ht += "<div class='ace_map-item' style='top:" +  (map_range(r.start.row ,0,editor.session.getLength()+nr_lines_vissible, 0,100)) + "%'></div>"
+					// luberth thinks, i do not know maybe better in % no px
+					console.log("rScale",rScale);
+					console.log("getFirstVisibleRow",editor.renderer.getFirstVisibleRow());
+					console.log("getLastVisibleRow",editor.renderer.getLastVisibleRow());
+					
+					lr = r.start.row
+				}
+			}
+		}
+		mc.innerHTML = ht
+		mr.style.top = map_range(e.getCursorPosition().row,0,editor.session.getLength()+50, 0,100)+"%"
+		console.log("rScale2",rScale);
+	}
+
+	// Vertical
+	let rScale = 1
+		
+	let vScale = 1
+	let vStart = -1
+
+	let vs = sc[0]
+	let tv = document.getElementById("ace_bar-v")    // tv is the gray view on scrollbar, should match the editor view position
+
+	vs.addEventListener("scroll", function (e) {//think it needes an offset here if editor does not start at top 0 of document
+	//	tv.style.top = (vs.scrollTop-document.getElementById("editor").offsetTop) * vScale + "px"
+		tv.style.top = (map_range(vs.scrollTop-document.getElementById("editor").offsetTop,0,vs.scrollHeight+vs.clientHeight,0,100))+"%"
+		//tv.style.top = (vs.scrollTop-e.offsetTop) * vScale + "px"
+
+	})
+
+	// Drag events
+	tv.addEventListener("pointerdown", function (e) {
+		tv.setPointerCapture(e.pointerId)
+		vStart = e.clientY
+    		//console.log("e.clientY",e.clientY);
+    		//console.log("mc.offsetTop+'px'",mc.offsetTop);
+    		console.log("offsetTop",document.getElementById("editor").offsetTop);
+				console.log();
+    
+	})
+
+	tv.addEventListener("pointermove", function (e) {
+		if (vStart != -1) {
+			vs.scrollTop += (e.clientY - vStart) / vScale
+			vStart = e.clientY
+		}
+	})
+
+	tv.addEventListener("pointerup", function (e) {
+		tv.releasePointerCapture(e.pointerId)
+		vStart = -1
+	})
+
+	tv.previousElementSibling.addEventListener("mousedown", function (e) {
+		// my editor does not start at zero because of my non editor buttons menu so offset needed
+     		vs.scrollTop = (e.clientY-document.getElementById("editor").offsetTop) / vScale
+	   	//vs.scrollTop = (e.clientY-e.offsetTop) / vScale
+
+	})
+
+	// Horizontal
+	let hScale = 1
+	let hStart = -1
+
+	let hs = sc[1]
+	let th = document.getElementById("ace_bar-h")
+/*
+	hs.addEventListener("scroll", function () {
+		th.style.left = (hs.scrollLeft * hScale) + gWidth + "px"
+	})
+
+	// Drag events
+	th.addEventListener("pointerdown", function (e) {
+		th.setPointerCapture(e.pointerId)
+		hStart = e.clientX
+	})
+
+	th.addEventListener("pointermove", function (e) {
+		if (hStart != -1) {
+			hs.scrollLeft += (e.clientX - hStart) / hScale
+			hStart = e.clientX
+		}
+	})
+
+	th.addEventListener("pointerup", function (e) {
+		th.releasePointerCapture(e.pointerId)
+		hStart = -1
+	})
+
+	th.previousElementSibling.addEventListener("mousedown", function (e) {
+		hs.scrollLeft = e.clientX / hScale
+	})
+*/
+	resizeScroll = function () {
+		vScale = vs.clientHeight / vs.scrollHeight
+		if (vs.clientHeight) {
+			tv.style.height = (vs.clientHeight * vScale) + "px"
+			showScroll(tv, true)
+		}
+		else {
+			showScroll(tv, false)
+		}
+/*
+		hScale = hs.clientWidth / hs.scrollWidth
+		if (hs.clientWidth) {
+			th.style.width = (hs.clientWidth * hScale) + "px"
+			th.style.left = (hs.scrollLeft * hScale) + gWidth + "px"
+			showScroll(th, true)
+		}
+		else {
+			showScroll(th, false)
+		}
+*/
+		rScale = e.renderer.lineHeight * vScale
+		console.log("e.renderer.lineHeight",e.renderer.lineHeight);
+		gWidth = e.renderer.gutterWidth
+		th.previousElementSibling.style.left = gWidth + "px"
+		showMapItems()
+	}
+
+	showScroll = function (e, s) {
+		d = (s) ? "" : "none"
+		e.style.display = d
+		e.previousElementSibling.style.display = d
+	}
+
+
+
+	resizeScroll() // luberth added this on startup scrollbar was not ok only after resize window it was ok
+}
--- docs/readme.md
+++ docs/readme.md
@@ -1,1 +1,57 @@
+# Ace Editor Custom Scrollbar 
+
+map search / highlight select on vertical scrollbar
+
+CSS Scrollbars for [Ace Editor](https://github.com/ajaxorg/ace)
+
+Ace demo https://ace.c9.io/build/kitchen-sink.html
+
+---
+ace kitchen-sink demo custom scrollbars did not work nice i think
+
+so searched and found this one that worked better i think
+
+changes to original ext-scrollbar.js https://github.com/ldijkman/Ace-Scrollbars/blob/master/src/ext-scrollbar.js
+
+treid to change
+
+offset if editor does not start at windowpos zero
+
+from pixel position to percent positions
+
+works a bit better on different font sizes
+
+Ace Editor Custom ScrollBars Demo https://ldijkman.github.io/randomnerd_esp32_wifi_manager/mrs/create_from_scratch/Insane%20in%20the%20Membrane/ACE_Editor_JumpMarks_ScrollMarks.html
+
+Ace Editor Custom ScrollBars Demo https://plnkr.co/edit/EYJWlWGjCdjNoui8?preview
+
+Ace Editor Custom ScrollBars Demo https://codepen.io/ldijkman/pen/ZEVXOEv
+
+---
+
+Simple to use CSS styled scrollbars:
+
+Just link to two files then initialise the scrollbars after initialising Ace.
+
+    <link rel="stylesheet" type="text/css" href="ext-scrollbar.min.css">
+    <script src="ext-scrollbar.min.js"></script>
+
+    <script>
+    // Ace initialisation
+    var editor = ace.edit("editor")
+    // Create new scrollbars
+    new AceScrollbars(editor)
+    ...
+    </script>
+### Added search/occurance map
+
+Ace Editor Custom ScrollBars Demo https://plnkr.co/edit/EYJWlWGjCdjNoui8?preview
+
+Ace Editor Custom ScrollBars Demo https://codepen.io/ldijkman/pen/ZEVXOEv
+
+![2023-09-17-083638_1920x1080_scrot](https://github.com/ldijkman/Ace-Scrollbars/assets/45427770/efe9dcb7-a4ea-4004-b846-27209076058d)
+
+---
+
+<img src="./screenshot.png">
 
